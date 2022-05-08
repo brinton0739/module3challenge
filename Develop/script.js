9be35a604343fcad6ad542d2b7971944ae9f656b
Brinton Cole <brintoncole@gmail.com>
2022-05-07 20:19:43 -0600

    wip commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+// for (initialize variable; evaluate condition; run after loop)
+// for (i = 0; i <= 10; i++) {
+//   alert(i)
+// }
 
 
 // Get references to the #generate element
@@ -15,3 +19,76 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  // prompt
+  // validate at least one of the following is true
+  var hasSpecialCharacters = promptYesOrNo("special characters");
+  var hasNumbers = promptYesOrNo("numbers");
+  var hasUpperCase = promptYesOrNo("upper case");
+  var hasLowerCase = promptYesOrNo("lower case");
+  
+  // validate
+  if (!hasSpecialCharacters && !hasNumbers && !hasUpperCase && !hasLowerCase) {
+    alert("You must answer yes to atleast one character type");
+    return;
+  }
+
+  var numberOfCharacters = promptNumberOfCharacters();
+
+  // generate password
+  return generateRandomPassword(numberOfCharacters, hasSpecialCharacters, hasNumbers, hasLowerCase, hasUpperCase);
+}
+
+// prompts for yes or no returnes true for yes or false for no
+function promptYesOrNo(question) {
+  for (validated = false; validated === false;) {
+    var answer = prompt("Should include " + question + "? Enter yes or no", "");
+    if (answer.toLowerCase() === "yes") {
+      return true;
+    } else if (answer.toLowerCase() === "no") {
+      return false;
+    }
+    alert("Please enter yes or no");
+  }
+}
+
+function promptNumberOfCharacters() {
+  for (;true;) {
+    var answer = prompt("please choose the length of your password, enter a number between 8 and 128");
+    if (answer >= 8 && answer <= 128) {
+      return answer;
+    }
+    alert("you must ender a number between 8 and 128");
+  }
+}
+
+function generateRandomPassword(numberOfCharacters, hasSpecialCharacters, hasNumbers, hasLowerCase, hasUpperCase) {
+  const specialCharacters = "!@#$%^&*";
+  const numberCharacters = "1234567890";
+  const lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  // create string to generate password
+  var randomString = "";
+  if (hasSpecialCharacters) {
+    randomString = randomString + specialCharacters;
+  }
+  if (hasNumbers) {
+    randomString = randomString + numberCharacters;
+  }
+  if (hasLowerCase) {
+    randomString = randomString + lowerCaseCharacters;
+  }
+  if (hasUpperCase) {
+    randomString = randomString + upperCaseCharacters;
+  }
+
+  var password = "";
+
+  for(var i = 0; i < numberOfCharacters; i++){
+    password = password + randomString[Math.floor(Math.random() * randomString.length)]
+  }
+  return password;
+}
